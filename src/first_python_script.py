ae170b22b6ebd8b2d3740e77ae783d6861dffa2a
yuedongxu <ydx@outlook.my>
2024-08-19 10:50:17 +0800

    Update first_python_script.py

--- src/first_python_script.py
+++ src/first_python_script.py
@@ -85,7 +85,7 @@ def main():
     end_time = next_day.replace(hour=15, minute=0, second=0, microsecond=0).strftime('%Y-%m-%d %H:%M')
     print(f"预定时间段：{begin_time} - {end_time}")
     data = {
-        'instrumentId': "1730",
+        'instrumentId': "1725", #instrumentId
         'beginTime': begin_time,
         'endTime': end_time,
         'remarks': '',
@@ -116,7 +116,7 @@ def main():
     # 获取当前时间
     current_time = datetime.now()
     # 设定目标时间段
-    start_time = current_time.replace(hour=2, minute=45, second=59, microsecond=0)
+    start_time = current_time.replace(hour=0, minute=59, second=59, microsecond=0)
     end_time = start_time + timedelta(seconds=2)  # 结束时间为 start_time 多 3 秒
     print("开始运行时间是：",start_time.hour),print("当前时间是：",current_time.hour)
     if start_time.hour != current_time.hour:
