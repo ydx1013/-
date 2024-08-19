67d3d2380b0a521d2209d48c268affa66b93f0b4
yuedongxu <ydx@outlook.my>
2024-08-19 17:37:29 +0800

    Update wanlei.py

--- src/wanlei.py
+++ src/wanlei.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import requests
+import time
 def wanlei():
   cookies = {
     'PHPSESSID': '6m8q7k25h1cjrah8c782i6id46',
@@ -13,8 +14,13 @@ def wanlei():
       'X-Requested-With': 'XMLHttpRequest',
   }
   
+  data = {
+      'MyKey': 'a3bf8ad5421091f9de92335e694b8cb0',
+      'UserId': 'dd6b42f5dee5cef8383cece0d42dc056',
+      'timestamp': int(time.time()),
+  }
   
-  response = requests.post('https://shop.biodog.cn//Center/Sign', cookies=cookies, headers=headers)
+  response = requests.post('https://appapi.biodog.cn//Center/Sign', cookies=cookies, headers=headers, data=data)
   # 将 response.text 从 Unicode 转换为 UTF-8 编码的字符串
   response_text_utf8 = response.text.encode('utf-8').decode('unicode-escape')
   print(response_text_utf8)
