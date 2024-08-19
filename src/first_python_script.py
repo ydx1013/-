94b50da232c9d0ec4398eb2b85cad5a40da31357
yuedongxu <ydx@outlook.my>
2024-08-19 12:25:57 +0800

    Update first_python_script.py

--- src/first_python_script.py
+++ src/first_python_script.py
@@ -4,6 +4,8 @@ import ddddocr
 import requests
 import sys
 import io
+import wanlei.py
+wanlei()
 # 改变标准输出的默认编码为UTF-8
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 from datetime import datetime, timedelta
